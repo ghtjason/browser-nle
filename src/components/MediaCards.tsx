5db6c30b0e2f959b149c90be6d712e7471abbe32
ghtjason <jasonshuhezhang@gmail.com>
2024-01-08 17:11:04 -0800

    much improved timeline card moving/resizing with snapping support

--- src/components/MediaCards.tsx
+++ src/components/MediaCards.tsx
@@ -12,7 +12,7 @@ import { IconDotsVertical } from "@tabler/icons-react";
 
 export default function MediaCard({ img }: { img: BaseMedia }) {
   return (
-    <Box width="175px" borderWidth={1} borderRadius="lg" overflow="hidden">
+    <Box width="140px" borderWidth={1} borderRadius="lg" overflow="hidden">
       <Image src={img.thumbnailURL} alt={img.name} />
       <Heading size="xs" isTruncated={true} padding="6px">
         {img.name}
@@ -34,10 +34,14 @@ export function TimelineMediaCard(props: IProps) {
   const [, , , , isPlaying] = useContext(PlayContext);
   const duration = props.media.end - props.media.start;
   const width = duration / 10;
-  // const offset = props.media.start / 10;
+  const [rightHighlighted, setRightHighlighted] = useState(false);
+  const [leftHighlighted, setLeftHighlighted] = useState(false);
+  const rightColor = rightHighlighted ? "#ECC94B" : "white";
+  const leftColor = leftHighlighted ? "#ECC94B" : "white";
   const outline = props.media === selectedCard ? "#ECC94B" : "#2D3748";
-  const [, refreshSnapTimes] = useContext(SnapTimesContext);
+  const [snapTimes, refreshSnapTimes] = useContext(SnapTimesContext);
   const [offset, setOffset] = useState(props.media.start / 10);
+  const [, setRender] = useState(0);
 
   if (offset != props.media.start / 10) setOffset(props.media.start / 10); // weird workaround for mouse move rendering
 
@@ -49,85 +53,147 @@ export function TimelineMediaCard(props: IProps) {
     }
   }
 
-  const handleMouseDown = useCallback(() => {
-    const handleMouseUp = () => {
-      document.removeEventListener("mousemove", handleMouseMove);
-      refreshSnapTimes();
-    };
-
-    const handleMouseMove = (e: MouseEvent) => {
-      const delta = Math.max(-props.media.start, e.movementX * 10);
-      props.media.start += delta;
-      props.media.end += delta;
-      setOffset(offset + delta / 10);
-      // weird, should only be used to rerender component but breaks otherwise?????
-    };
-
-    if (isPlaying) return;
-    document.addEventListener("mousemove", handleMouseMove);
-    document.addEventListener("mouseup", handleMouseUp, { once: true });
-  }, [isPlaying, offset, props.media, refreshSnapTimes]);
-
-  const handleDragRight = useCallback(() => {
-    const handleMouseUp = () => {
-      document.removeEventListener("mousemove", handleMouseMove);
-      document.body.style.cursor = "default";
-      refreshSnapTimes();
-    };
-
-    const handleMouseMove = (e: MouseEvent) => {
-      let delta = e.movementX * 10;
-      if (props.media instanceof VideoMediaTimeline) {
-        const maxDelta =
-          props.media.media.duration * 1000 -
-          props.media.offsetStart -
-          (props.media.end - props.media.start);
-        delta = Math.min(maxDelta, delta);
-        props.media.end += delta;
-      } else props.media.end += delta;
-      if (props.media.end < props.media.start + 100) {
-        // min 100ms duration
-        props.media.end = props.media.start + 100;
-        setOffset(100);
-      } else setOffset(offset + delta / 10);
-      // weird, should only be used to rerender component but breaks otherwise?????
-    };
-
-    if (isPlaying) return;
-    document.body.style.cursor = "e-resize";
-    document.addEventListener("mousemove", handleMouseMove);
-    document.addEventListener("mouseup", handleMouseUp, { once: true });
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isPlaying]);
-
-  const handleDragLeft = useCallback(() => {
-    const handleMouseUp = () => {
-      document.body.style.cursor = "default";
-      document.removeEventListener("mousemove", handleMouseMove);
-      refreshSnapTimes();
-    };
-
-    const handleMouseMove = (e: MouseEvent) => {
-      let delta = Math.max(-props.media.start, e.movementX * 10); // max extension to 0
-      const maxDelta = props.media.end - props.media.start - 100;
-      delta = Math.min(delta, maxDelta);
-      if (props.media instanceof VideoMediaTimeline) {
-        const minDelta = -props.media.offsetStart;
-        delta = Math.max(minDelta, delta);
-        props.media.offsetStart += delta;
-        props.media.start += delta;
+  function snapToEdgeDiff(
+    start: number,
+    end: number,
+    ignoreStart: number,
+    ignoreEnd: number
+  ) {
+    function snapToEdge(n: number) {
+      for (const snap of snapTimes) {
+        if (snap == ignoreStart || snap == ignoreEnd) continue;
+        if (Math.abs(n - snap) < 100) return snap;
       }
-      else props.media.start += delta;
-      setOffset(offset + delta);
-      // weird, should only be used to rerender component but breaks otherwise?????
-    };
+      return n;
+    }
+    let diff = start - snapToEdge(start);
+    if (diff == 0) diff = end - snapToEdge(end);
+    return diff;
+  }
 
-    if (isPlaying) return;
-    document.body.style.cursor = "e-resize";
-    document.addEventListener("mousemove", handleMouseMove);
-    document.addEventListener("mouseup", handleMouseUp, { once: true });
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isPlaying]);
+  const handleMouseDown = useCallback(
+    (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+      const diff = e.pageX - props.media.start / 10;
+      const ogStart = props.media.start;
+      const ogEnd = props.media.end;
+      const handleMouseUp = () => {
+        document.removeEventListener("mousemove", handleMouseMove);
+        refreshSnapTimes();
+      };
+
+      const handleMouseMove = (e: MouseEvent) => {
+        let newStart = (e.pageX - diff) * 10;
+        let highlighted = false;
+        if (newStart <= 0) {
+          newStart = 0;
+          highlighted = true;
+        } else setLeftHighlighted(false);
+        const newEnd = newStart + props.media.end - props.media.start;
+        const snapDiff = snapToEdgeDiff(newStart, newEnd, ogStart, ogEnd);
+        props.media.start = newStart - snapDiff;
+        props.media.end = newEnd - snapDiff;
+        setLeftHighlighted(highlighted);
+        setRender(newStart);
+        // weird, should only be used to rerender component but breaks otherwise?????
+      };
+
+      if (isPlaying) return;
+      document.addEventListener("mousemove", handleMouseMove);
+      document.addEventListener("mouseup", handleMouseUp, { once: true });
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [isPlaying]
+  );
+
+  const handleDragRight = useCallback(
+    (e: React.MouseEvent<SVGElement, MouseEvent>) => {
+      const diff = e.pageX - props.media.end / 10;
+      const ogEnd = props.media.end;
+      const handleMouseUp = () => {
+        document.body.style.cursor = "default";
+        setRightHighlighted(false);
+        document.removeEventListener("mousemove", handleMouseMove);
+        refreshSnapTimes();
+      };
+
+      const handleMouseMove = (e: MouseEvent) => {
+        let newEnd = (e.pageX - diff) * 10;
+        let highlighted = false;
+        if (newEnd <= props.media.start + 100) {
+          newEnd = props.media.start + 100;
+          highlighted = true;
+        }
+        if (props.media instanceof VideoMediaTimeline) {
+          const maxEnd =
+            props.media.start +
+            props.media.media.duration * 1000 -
+            props.media.offsetStart;
+          if (newEnd >= maxEnd) {
+            newEnd = maxEnd;
+            highlighted = true;
+          }
+        }
+        const snapDiff = snapToEdgeDiff(newEnd, newEnd, ogEnd, ogEnd);
+        props.media.end = newEnd - snapDiff;
+        setRightHighlighted(highlighted);
+        setRender(newEnd);
+      };
+
+      if (isPlaying) return;
+      document.body.style.cursor = "e-resize";
+      document.addEventListener("mousemove", handleMouseMove);
+      document.addEventListener("mouseup", handleMouseUp, { once: true });
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [isPlaying]
+  );
+
+  const handleDragLeft = useCallback(
+    (e: React.MouseEvent<SVGElement, MouseEvent>) => {
+      const ogStart = props.media.start;
+      const diff = e.pageX - props.media.start / 10;
+      const handleMouseUp = () => {
+        document.body.style.cursor = "default";
+        setLeftHighlighted(false);
+        document.removeEventListener("mousemove", handleMouseMove);
+        refreshSnapTimes();
+      };
+
+      const handleMouseMove = (e: MouseEvent) => {
+        let newStart = (e.pageX - diff) * 10;
+        const lastStart = props.media.start;
+        let highlighted = false;
+        if (newStart <= 0) {
+          newStart = 0;
+          highlighted = true;
+        } else if (newStart >= props.media.end - 100) {
+          newStart = props.media.end - 100;
+          highlighted = true;
+        }
+        if (props.media instanceof VideoMediaTimeline) {
+          const minStart = props.media.start - props.media.offsetStart;
+          if (newStart <= minStart) {
+            newStart = minStart;
+            highlighted = true;
+          }
+          const snapDiff = snapToEdgeDiff(newStart, newStart, ogStart, ogStart);
+          props.media.offsetStart += newStart - lastStart - snapDiff;
+        }
+        const snapDiff = snapToEdgeDiff(newStart, newStart, ogStart, ogStart);
+        props.media.start = newStart - snapDiff;
+        setLeftHighlighted(highlighted);
+        setRender(newStart);
+      };
+
+      if (isPlaying) return;
+      document.body.style.cursor = "e-resize";
+      document.addEventListener("mousemove", handleMouseMove);
+      document.addEventListener("mouseup", handleMouseUp, { once: true });
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [isPlaying]
+  );
 
   return (
     <div
@@ -147,6 +213,7 @@ export function TimelineMediaCard(props: IProps) {
         paddingRight="2px"
         onMouseDown={handleDragRight}
         cursor="e-resize"
+        color={rightColor}
       />
       <Icon
         as={IconDotsVertical}
@@ -157,6 +224,7 @@ export function TimelineMediaCard(props: IProps) {
         paddingLeft="2px"
         onMouseDown={handleDragLeft}
         cursor="w-resize"
+        color={leftColor}
       />
 
       <Box
